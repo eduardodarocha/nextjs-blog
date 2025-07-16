5ebf0d491adf8cfba16bdb765ec60011a38eefb3
eduardodarocha <eduardomfrocha@gmail.com>
2025-07-16 14:27:44 -0300

    Add script with google analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import "../styles/global.css"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-PH31J3DLE1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-PH31J3DLE1');
+      </script>
+      <Component {...pageProps} />
+    </>
+  );
 }
