383c3aac90e3483948dc47f25210e44000490481
eduardodarocha <eduardomfrocha@gmail.com>
2026-08-08 18:04:09 -0300

    new design

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import { Inter, JetBrains_Mono } from "@next/font/google";
+import { Montserrat, JetBrains_Mono } from "@next/font/google";
 import "../styles/global.css";
 
-const inter = Inter({ subsets: ["latin"], display: "swap" });
+const montserrat = Montserrat({ subsets: ["latin"], display: "swap" });
 const jetbrainsMono = JetBrains_Mono({ subsets: ["latin"], display: "swap" });
 
 export default function App({ Component, pageProps }) {
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }) {
     <>
       <style jsx global>{`
         :root {
-          --font-inter: ${inter.style.fontFamily};
+          --font-montserrat: ${montserrat.style.fontFamily};
           --font-jetbrains-mono: ${jetbrainsMono.style.fontFamily};
         }
       `}</style>
